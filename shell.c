454bbfa8c7ab18089c7acc47306c2c672f1751a1
may@ubuntu <626839049@qq.com>
2022-04-22 23:52:06 +0800

    pipe

--- shell.c
+++ shell.c
@@ -8,7 +8,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-
 typedef struct command
 {
 	char buf[1024];
@@ -20,7 +19,9 @@ typedef struct command
 
 void normal(command *cmd)
 {
+	printf("buf: %s, argv[1]: %s", cmd->buf, cmd->argv[0]);
 	execvp(cmd->buf, cmd->argv);
+	printf("problem in normal\n");
 	perror("exec");
 	exit(1);
 }
@@ -34,13 +35,13 @@ void redirect_out(command *cmd)
 	dup2(fd, STDOUT_FILENO);
 	close(fd);		
 	execvp(cmd->buf, cmd->argv);
+	printf("problem in redirect_out\n");
 	perror("exec");
 	exit(1);
 }
 
 void redirect_in(command *cmd)
 {
-
 	if(cmd->argv[cmd->pos + 1] == NULL) perror("command < [option]"), exit(1);
 	cmd->argv[cmd->pos] = NULL;
 	int fd;
@@ -48,6 +49,7 @@ void redirect_in(command *cmd)
 	dup2(fd, STDIN_FILENO);
 	close(fd);
 	execvp(cmd->buf, cmd->argv);
+	printf("problem in redirect_in\n");
 	perror("exec");
 	exit(1);
 }
@@ -61,10 +63,10 @@ void redirect_append(command *cmd)
 	dup2(fd, STDOUT_FILENO);
 	close(fd);
 	execvp(cmd->buf, cmd->argv);
+	printf("problem in redirect_append\n");
 	perror("exec");
 	exit(1);
 }
-
 
 void execute(command *cmd)
 {
@@ -83,45 +85,40 @@ void execute(command *cmd)
 	wait(NULL);
 }
 
+void pipe_exec(command *cmd)
+{
+	switch(cmd->op)
+	{
+		case 1: redirect_in(cmd);
+		case 2: redirect_out(cmd);
+		case 3: redirect_append(cmd);
+		default: normal(cmd);
+	}
+}
+
 void execute_pipe(command *cmd, command *cmd_next)
 {
 	int fd[2];
-	pid_t pid1, pid2;
-
-	if(pipe(fd) < 0)  perror("pipe"), exit(1);
-
-	if((pid1 = fork()) < 0) perror("fork"), exit(1);
-	if(pid1 == 0)
+	if(pipe(fd) < 0) perror("pipe"),exit(1);
+	pid_t pid;
+	if((pid = fork()) < 0) perror("fork"), exit(1);
+	if(pid == 0)
 	{
 		close(fd[0]);
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[1]);
-		switch(cmd->op)
-		{
-			case 1: redirect_in(cmd);
-			case 2: redirect_out(cmd);
-			case 3: redirect_append(cmd);
-			default: normal(cmd);
-		}
+		execute(cmd);
+		exit(1);
 	}
-	if((pid2 = fork()) < 0) perror("fork"), exit(1);
-	if(pid2 == 0)
+	if(pid > 0)
 	{
+		int status;
+		waitpid(pid, &status, 0);
 		close(fd[1]);
-		dup2(fd[0], STDOUT_FILENO);
+		dup2(fd[0], STDIN_FILENO);
 		close(fd[0]);
-		switch(cmd_next->op)
-		{
-			case 1: redirect_in(cmd_next);
-			case 2: redirect_out(cmd_next);
-			case 3: redirect_append(cmd_next);
-			default: normal(cmd_next);
-		}
+		execute(cmd_next);
 	}
-	close(fd[0]);
-	close(fd[1]);
-	wait(NULL);	
-	wait(NULL);
 }
 
 void parser(command *cmd)
@@ -152,7 +149,6 @@ void parser(command *cmd)
 	}
 }
 
-
 void check(command *cmd)
 {
 	int pipe = 0;
@@ -161,8 +157,9 @@ void check(command *cmd)
 	{
 		if(cmd->buf[i] == '|')
 		{
-			strcpy(cmd_next->buf, cmd->buf + i + 1);
-			cmd->buf[i] = 0;
+			cmd->buf[i++] = 0;
+			while(isspace(cmd->buf[i])) i++;
+			strcpy(cmd_next->buf, cmd->buf + i);		
 			pipe++;
 		}
 	}
@@ -175,7 +172,16 @@ void check(command *cmd)
 	{
 		parser(cmd);
 		parser(cmd_next);
-		execute_pipe(cmd, cmd_next);
+		pid_t pid;
+		if((pid = fork()) < 0) perror("fork"), exit(1);
+		if(pid == 0)
+		{
+			execute_pipe(cmd, cmd_next);
+			exit(1);
+		}
+		if(pid > 0)
+			wait(NULL);
+
 	}
 	return;
 }
@@ -193,7 +199,6 @@ int main()
 			printf("My shell# ");
 			while(getchar() != '\n');
 		}
-
 		check(cmd);
 	}
 
